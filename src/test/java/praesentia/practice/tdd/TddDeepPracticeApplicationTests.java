aeffc439bdca116015814833bbfc755435d608d6
praesentia <yangkwonmo0916@gmail.com>
2025-10-06 15:08:31 +0900

    init. settings files

---
+++ src/test/java/praesentia/practice/tdd/TddDeepPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package praesentia.practice.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDeepPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
