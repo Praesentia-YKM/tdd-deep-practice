ecacee914184ff294bc40d1840749586f213ca87
praesentia <yangkwonmo0916@gmail.com>
2025-10-06 17:17:46 +0900

    "feat. 판매자 회원가입 api test코드 작성" - tdd 사이클의 첫 주기생성 - api가 없기 때문에 404로 응답하는 미통과(red)형태 결과

---
+++ src/test/java/test/commerce/api/seller/signup/POST_specs.java
@@ -0,0 +1,45 @@
+package test.commerce.api.seller.signup;
+
+import commerce.CommerceApiApp;
+import commerce.command.CreateSellerCommand;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static test.commerce.EmailGenerator.generateEmail;
+import static test.commerce.UsernameGenerator.generateUsername;
+
+@SpringBootTest(
+    classes = { CommerceApiApp.class },
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@DisplayName("POST /seller/signUp")
+public class POST_specs {
+
+    @Test
+    void 올바르게_요청하면_204_No_Content_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            generateEmail(),
+            generateUsername(),
+            "password",
+            generateEmail()
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
+}
