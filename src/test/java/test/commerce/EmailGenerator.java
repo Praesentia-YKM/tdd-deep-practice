7e80dd098036308c99e11b44e0c52417d3037446
praesentia <yangkwonmo0916@gmail.com>
2025-10-07 20:10:21 +0900

    "feat: 판매자 회원가입 테스트 코드 요구사항 반영" - 고정된 파라미터가 주는 거짓양성 해결을 위한 임의의 데이터 생성기를 만든다. - uuid기반 항상 새로운 이메일,id를 만드는 방식

--- src/test/java/test/commerce/EmailGenerator.java
+++ src/test/java/test/commerce/EmailGenerator.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 public class EmailGenerator {
 
+    // uuid기반 항상 새로운 이메일을 만드는 방식
+    // 테스트 모듈 안에서만 쓰임(트랜잭션 롤백 방식으로 할 수 있지만 성능 상 이 방식이 더 좋다고 판단)
     public static String generateEmail() {
         return UUID.randomUUID() + "@test.com";
     }
